99ea89bf35da71b84cd4cdfee9a6ad0f8bbaf85b
Sajuuk <Sajuuk@79d7a13a-6d1f-43f7-b041-d37804f8f9f8>
2011-04-16 15:02:04 +0000

    Adding the Upgrader File. Used to update your version to a newer version.

---
+++ install/upgrade.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ImprovCMS Project
+    
+    Name: Upgrader
+    Description: This file is used to upgrade an older version of Improv to a newer and more secure version.
+
+    Author: Kyuubi
+
+    Copyright (c) 2010 Kyuubi and Improv Software Group
+	All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+require_once("./resources/functions_upgrade.php");
+error_reporting(0);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+	<link rel="stylesheet" type="text/css" href="./style.css" media="screen" />
+	<title>ImprovCMS Upgrader</title>
+</head>
+<body>
+
+   <div id="wrapper">
+   
+		<div id="header">
+			<img src="../images/logo.png" alt="ImprovCMS Upgrader" />		 
+		</div>
+		
+		<?php
+		$action = $_REQUEST['action'];	 
+
+		if(file_exists('lock'))
+		{
+			$action = "locked";
+		}
+		
+		switch ($action)
+		{
+			case 'version':			
+			echo "<div id='navigation'>Overview - <b>Select Version</b> - Begin Upgrade - Updating AdminCP - Updating Settings - Updating Styles - Finish</div>";			
+			echo "<div id='content'>";
+			version();
+			break;			
+			
+			case 'update':			
+			echo "<div id='navigation'>Overview - Select Version - <b>Begin Upgrade</b> - Updating AdminCP - Updating Settings - Updating Styles - Finish</div>";			
+			echo "<div id='content'>";
+			update();
+			break;		
+			
+			case 'admin_update':			
+			echo "<div id='navigation'>Overview - Select Version - Begin Upgrade - <b>Updating AdminCP</b> - Updating Settings - Updating Style - Finish</div>";			
+			echo "<div id='content'>";
+			admin_update();
+			break;
+			
+			case 'settings_update':			
+			echo "<div id='navigation'>Overview - Select Version - Begin Upgrade - Updating AdminCP - <b>Updating Settings</b> - Updating Style - Finish</div>";			
+			echo "<div id='content'>";
+			settings_update();
+			break;
+			
+			case 'style_update':			
+			echo "<div id='navigation'>Overview - Select Version - Begin Upgrade - Updating AdminCP - Updating Settings - <b>Updating Style</b> - Finish</div>";			
+			echo "<div id='content'>";
+			style_update();
+			break;
+			
+			case 'finish':			
+			echo "<div id='navigation'>Overview - Select Version - Begin Upgrade - Updating AdminCP - Updating Settings - Updating Style - <b>Finish</b></div>";			
+			echo "<div id='content'>";
+			finish();
+			break;	
+			
+			case 'locked':
+			echo "<div id='navigation'><b>Error - Upgrader Locked</b></div>";			
+			echo "<div id='content'>";
+			locked();
+			break;
+			
+			default:			
+			echo "<div id='navigation'><b>Overview</b> - Select Version - Begin Upgrade - Updating AdminCP - Updating Settings - Updating Styles - Finish</div>";			
+			echo "<div id='content'>";
+			overview();
+			break;
+		}
+		
+		function overview();
+		{
+			$next = "version";
+			
+			//
+
+?>
