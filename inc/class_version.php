b10f2a2469d5f2147ab79a5919d5a0e3d17e2cbc
Sajuuk <Sajuuk@79d7a13a-6d1f-43f7-b041-d37804f8f9f8>
2011-04-17 14:56:08 +0000

    New class for version data.

---
+++ inc/class_version.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ImprovCMS Project
+    
+    Name: Version Class
+    Description: This file contains methods for determining latest and current version.
+
+    Author: Kyuubi
+
+    Copyright (c) 2010 Kyuubi and Improv Software Group
+	All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class version
+{
+	function latest()
+	{
+		$latest = "Alpha 3 Development";
+	}
+	function code()
+	{
+		$code = "003";
+	}
+	function current()
+	{
+		$current = // add in the code to find out current version here.
+	}
+}
