9db32c781292df0d4df94ec0f49b1ee673d81a9e
Sajuuk <Sajuuk@79d7a13a-6d1f-43f7-b041-d37804f8f9f8>
2011-05-14 18:16:27 +0000

    Fixes AdminCP Logout Not Working (fixes:#20)

--- admin/logout.php
+++ admin/logout.php
@@ -5,14 +5,13 @@ define("IN_ADMIN","1");
 require_once('../global.php');
 // Before the user can do anything here, check if the user is actually an administrator
 if (!$is_admin) {
-	error("You do not have permission to view this page");
-	exit;
+	die($error->perms(20));
 }
 // Eat the cookies
 setcookie("madminsession","",time()-3600);
 setcookie("mcmsuid","",time()-3600);
 // Remove the session.
-$db->query("UPDATE ".TABLE_PREFIX."users SET adminsession='0' WHERE uid='{$mcms->user['uid']}'");
+$db->query("UPDATE ".TABLE_PREFIX."users SET adminsession='0' WHERE uid='{$imp->user['uid']}'");
 // Redirect to the index page.
 redirect("./index.php");
 ?>
@@ -20,7 +19,7 @@ redirect("./index.php");
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>ACP Login | MillionCMS</title>
+<title>ACP Login | ImprovCMS</title>
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 <link rel="stylesheet" type="text/css" href="css/login_style.css" title="style" media="screen" />
@@ -53,8 +52,8 @@ redirect("./index.php");
 				<img align="absmiddle" src="images/loader_bar.gif"><br />&nbsp;Processing your Request...
 			</div>
 		</div>
-		<center><a href="<?php echo $mcms->settings['homeurl'] ?>"><img src="images/logo.png" alt="" width="312" height="600" /></a><br /></center>
-		<div id="copyright" align="center"><span style="color:#FFF; margin:3px 0;">Copyright&copy; 2010 MillionCMS - <a href="http://millioncms.com">Home Page</a> - <a href="http://millioncms.com/forums">Support</a></span></div>
+		<center><a href="<?php echo $imp->settings['homeurl'] ?>"><img src="images/logo.png" alt="" width="312" height="600" /></a><br /></center>
+		<div id="copyright" align="center"><span style="color:#FFF; margin:3px 0;">Copyright&copy; 2010 ImprovCMS - <a href="http://improvcms.com">Home Page</a> - <a href="http://improvcms.com/forums">Support</a></span></div>
 	</div>
 </body>
 </html>
