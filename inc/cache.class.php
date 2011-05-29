e1c8c9e3bf77355b60939eb960bc9591879ae0ce
Sajuuk <Sajuuk@79d7a13a-6d1f-43f7-b041-d37804f8f9f8>
2011-05-29 15:29:22 +0000

    Adding the new Cache System class to replace our outdated functions.

---
+++ inc/cache.class.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ImprovCMS Project
+    
+    Name: Cache Class
+    Description: This file contains data for the Cache System.
+
+    Author: Kyuubi
+
+    Copyright (c) 2010 Kyuubi and Improv Software Group
+	All Rights Reserved
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Stops unauthorised users accessing this...
+if(!defined("IN_IMPROV"))
+{
+	die("Access Denied");
+}
+
+class cache
+{
+	function write()
+	{
+	
+	}
+	
+	function read()
+	{
+	
+	}
+	
+	function drop()
+	{
+	
+	}
+	
+	function rebuild()
+	{
+	
+	}
+}
+?>
